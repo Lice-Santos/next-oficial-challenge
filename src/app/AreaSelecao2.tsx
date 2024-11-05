4ae1c5fe6ef37cd5b83b1270da9b75764367240d
Anne Rezendes <annerezendesm@gmail.com>
2024-11-04 22:16:46 -0300

    correção novamente

--- src/app/AreaSelecao2.tsx
+++ src/app/AreaSelecao2.tsx
@@ -11,7 +11,7 @@ export default function AreaSelecao2({inicioConteudo, finalConteudo, conteudoMar
             <div className="lado2">
                 <h3>{inicioConteudo}<strong>{conteudoMarcado}</strong></h3>
                 <p>{finalConteudo}</p>
-                <p className="topico"><span>1</span>SIMPLES de se utilizar</p>
+                <p className="topico"><span>1</span>FÁCIL de usar</p>
                 <p className="topico"><span>2</span>RÁPIDO de fazer</p>
                 <p className="topico"><span>3</span>ASSERTIVO no resultado</p>
             </div>
