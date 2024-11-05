81632d731715496a25039c917bd236c12b3b9a4f
Alice Santos <86413853+Lice-Santos@users.noreply.github.com>
2024-11-05 09:20:33 -0300

    Update page.tsx

--- src/app/carros/[id]/page.tsx
+++ src/app/carros/[id]/page.tsx
@@ -5,10 +5,15 @@ import { CarroProps } from "@/app/types";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-// A função agora é assíncrona para lidar com os parâmetros de rota
-export default async function Carro({ params }: { params: { id: string } }) {
+interface PageProps {
+    params: {
+        id: number;
+    };
+}
+
+export default function Carro({ params }: PageProps) {
     const navigate = useRouter();
-    const id = parseInt(params.id); // Convertendo para número
+    const id = params.id;
 
     const [carro, setCarro] = useState<CarroProps>({
         id: 0,
@@ -22,13 +27,12 @@ export default async function Carro({ params }: { params: { id: string } }) {
     useEffect(() => {
         const chamadaApi = async () => {
             const response = await fetch(`http://localhost:8080/carro/${id}`);
-            const data = await response.json();
-            setCarro(data);
-            console.log(data);
-        };
-        chamadaApi();
-    }, [id]);
-
+            const data = await response.json()
+            setCarro(data)
+            console.log(data)
+        }
+        chamadaApi()
+    }, [id])
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setCarro((prevState) => ({ ...prevState, [name]: value }));
@@ -39,13 +43,13 @@ export default async function Carro({ params }: { params: { id: string } }) {
 
         try {
             const cabecalho = {
-                method: "PUT",
+                method: 'PUT',
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(carro),
             };
             const response = await fetch(`http://localhost:8080/carro/${carro.id}`, cabecalho);
             if (response.ok) {
-                alert("Carro atualizado com sucesso");
+                alert('Carro atualizado com sucesso');
                 setCarro({
                     id: 0,
                     placa: "",
@@ -54,7 +58,7 @@ export default async function Carro({ params }: { params: { id: string } }) {
                     ano: 0,
                     quilometragem: 0,
                 });
-                navigate.push("/carros");
+                navigate.push('/carros');
             } else {
                 alert("Erro ao atualizar o carro");
             }
@@ -66,11 +70,11 @@ export default async function Carro({ params }: { params: { id: string } }) {
     const handleDelete = async () => {
         try {
             const response = await fetch(`http://localhost:8080/carro/${carro.id}`, {
-                method: "DELETE",
+                method: 'DELETE',
             });
             if (response.ok) {
-                alert("Carro deletado com sucesso");
-                navigate.push("/carros");
+                alert('Carro deletado com sucesso');
+                navigate.push('/carros');
             } else {
                 alert("Erro ao deletar o carro");
             }
@@ -83,6 +87,70 @@ export default async function Carro({ params }: { params: { id: string } }) {
         <MainFormCrud className="grow p-5">
             <h1>Carro</h1>
             <form className="w-1/3 m-auto p-2 border border-indigo-950 rounded-md" onSubmit={handleSubmit}>
+                <div className="flex flex-col p-2">
+                    <label className="text-gray-700" htmlFor="idplaca">Placa</label>
+                    <input
+                        className="border border-gray-700 p-1 rounded-md"
+                        type="text"
+                        name="placa"
+                        id="idplaca"
+                        onChange={handleChange}
+                        value={carro.placa}
+                    />
+                </div>
+                <div className="flex flex-col p-2">
+                    <label className="text-gray-700" htmlFor="idmarca">Marca</label>
+                    <input
+                        className="border border-gray-700 p-1 rounded-md"
+                        type="text"
+                        name="marca"
+                        id="idmarca"
+                        onChange={handleChange}
+                        value={carro.marca}
+                    />
+                </div>
+                <div className="flex flex-col p-2">
+                    <label className="text-gray-700" htmlFor="idmodelo">Modelo</label>
+                    <input
+                        className="border border-gray-700 p-1 rounded-md"
+                        type="text"
+                        name="modelo"
+                        id="idmodelo"
+                        onChange={handleChange}
+                        value={carro.modelo}
+                    />
+                </div>
+                <div className="flex flex-col p-2">
+                    <label className="text-gray-700" htmlFor="idano">Ano</label>
+                    <input
+                        className="border border-gray-700 p-1 rounded-md"
+                        type="number"
+                        name="ano"
+                        id="idano"
+                        onChange={handleChange}
+                        value={carro.ano}
+                    />
+                </div>
+                <div className="flex flex-col p-2">
+                    <label className="text-gray-700" htmlFor="idquilometragem">Quilometragem</label>
+                    <input
+                        className="border border-gray-700 p-1 rounded-md"
+                        type="number"
+                        name="quilometragem"
+                        id="idquilometragem"
+                        onChange={handleChange}
+                        value={carro.quilometragem}
+                    />
+                </div>
+                <div className="btns">
+
+                <button className="bg-green-700" type="submit">
+                    Atualizar Carro
+                </button>
+                <button className="bg-red-700" type="button" onClick={handleDelete}>
+                    Deletar Carro
+                </button>
+                </div>
             </form>
         </MainFormCrud>
     );
